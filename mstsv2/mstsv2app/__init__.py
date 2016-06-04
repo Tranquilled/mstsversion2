9385429109dd6a0d5b9582d5c4564711bdbe82f7
Olivier Simard-Morissette <olivier.simard-morissette@mail.mcgill.ca>
2016-06-04 16:55:23 -0400

    Updated structure for blueprint, will start work on main

--- mstsv2/mstsv2app/__init__.py
+++ mstsv2/mstsv2app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from db import db
 
-# importing blueprint objects
-
 
 
 
@@ -12,10 +10,20 @@ app = Flask(__name__)
 app.config.from_object('settings')
 
 
+
+
+
+
+
 # Initializes Flask SQLAlchemy
 db.init_app(app)
 db.app = app
-db.create_all()
 
 
 # Uses Blueprint to import different modules
+from blogs.routes import blogs_blueprint
+
+
+app.register_blueprint(blogs_blueprint,url_prefix='api/blogs')
+
+db.create_all()
